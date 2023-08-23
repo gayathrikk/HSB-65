75bbb74bbeb93b8e7e92cfb74a4303eaa8b5f353
gayathrikk <gayathri@htic.iitm.ac.in>
2023-08-23 14:04:04 +0530

    first commit

---
+++ src/main/java/com/apollo2/HSB_65/App.java
@@ -0,0 +1,13 @@
+package com.apollo2.HSB_65;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
